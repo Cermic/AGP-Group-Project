f2835ea31ebfd30d5a80e9c20ae13e462365c77b
Jack Smith <streamlined50@hotmail.com>
2017-11-16 18:34:48 +0000

    Basic scene from tutorial with assets packed into folders appropriately.

--- AGP Group Project/main.cpp
+++ AGP Group Project/main.cpp
@@ -1,42 +1,44 @@
-// This program is a placeholder at present for the AGP Group Project
-// Project Members: 
-// Jack D Smith - B00308927, Greg Smith - B00308929 
+// Light mapping renderer
+// This demo will load and render a scene, some mesh objects, a camera for the viewer and a skybox.
+// Most of the OpenGL code for dealing with buffer objects, etc has been moved to a 
+// utility library, to make creation and display of mesh objects as simple as possible
 
 #if _DEBUG
 #pragma comment(linker, "/subsystem:\"console\" /entry:\"WinMainCRTStartup\"")
 #endif
 
-#include <GL/glew.h> // Access to base openGL library functions
-#include <SDL_ttf.h> // needs the extension to use the True type fonts
-#include "rt3d.h"
+#include "Game\Game.h"
+#include "SDL.h"
+#include <SDL_ttf.h>
+#include "Game\Utilities\rt3d.h"
 
-using namespace std;
+Game scene1;
 
 // Set up rendering context
 SDL_Window * setupRC(SDL_GLContext &context) {
 	SDL_Window * window;
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) // Initialize video
-		rt3d::exitFatalError("Unable to initialize SDL");
-
-	// Request an OpenGL 3.0 context.
-
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) // Initialize video
+        rt3d::exitFatalError("Unable to initialize SDL"); 
+	  
+    // Request an OpenGL 3.0 context.
+	
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); 
 
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);  // double buffering on
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8); // 8 bit alpha buffering
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
 	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4); // Turn on x4 multisampling anti-aliasing (MSAA)
-
-													   // Create 1920x1080 window
-	window = SDL_CreateWindow("AGP Group Project Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+ 
+    // Create 1920x1080 window
+    window = SDL_CreateWindow("Light Mapping", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
 	if (!window) // Check window was created OK
-		rt3d::exitFatalError("Unable to create window");
-
-	context = SDL_GL_CreateContext(window); // Create opengl context and attach to window
-	SDL_GL_SetSwapInterval(1); // set swap buffers to sync with monitor's vertical refresh rate
+        rt3d::exitFatalError("Unable to create window");
+ 
+    context = SDL_GL_CreateContext(window); // Create opengl context and attach to window
+    SDL_GL_SetSwapInterval(1); // set swap buffers to sync with monitor's vertical refresh rate
 	return window;
 }
 
@@ -64,18 +66,21 @@ bool handleSDLEvent(SDL_Event const &sdlEvent)
 // Initialiser
 void init(void) {
 
+	scene1.initialise();
 }
 
 // Update method
 void update(void) {
-
+	
+		scene1.update();
 }
 
 // Draw method
 void draw(SDL_Window * window) {
 
+	scene1.render(window);
 
-}
+	}
 
 // Program entry point - SDL manages the actual WinMain entry point for us
 int main(int argc, char *argv[]) {
@@ -83,7 +88,7 @@ int main(int argc, char *argv[]) {
 	SDL_GLContext glContext; // OpenGL context handle
 	hWindow = setupRC(glContext); // Create window and render context 
 
-								  // Required on Windows *only* init GLEW to access OpenGL beyond 1.1
+	// Required on Windows *only* init GLEW to access OpenGL beyond 1.1
 	glewExperimental = GL_TRUE;
 	GLenum err = glewInit();
 	if (GLEW_OK != err) { // glewInit failed, something is seriously wrong
@@ -104,15 +109,8 @@ int main(int argc, char *argv[]) {
 		draw(hWindow); // call the draw function
 	}
 
-	SDL_GL_DeleteContext(glContext);
-	SDL_DestroyWindow(hWindow);
-	SDL_Quit();
-	return 0;
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(hWindow);
+    SDL_Quit();
+    return 0;
 }
-
-/*
-
-	Change 1. I MADE THIS CHANGE TO SEE IT ON THE DEV BRANCH IN GIT
-	Change 2. Another change
-
-*/
